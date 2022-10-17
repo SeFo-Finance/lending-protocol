37d66fba9bbf2e3a0ad08f0a6d7c36181ef4846f
An-Ting Hsu <andyhsu10@gmail.com>
2022-10-17 14:22:13 +0800

    feat: testing verification functions

--- tests/controller_test.ts
+++ tests/controller_test.ts
@@ -1,49 +1,129 @@
 import { Clarinet, Chain, Account } from 'https://deno.land/x/clarinet@v1.0.2/index.ts'
-import { 
-    getExp,
-    mulExp,
-    mulExp3,
-    divExp,
-    mulScalarTruncate,
-    mulScalarTruncateAddUint,
-    truncate,
+import {
+  isController,
+  mintVerify,
+  redeemVerify,
+  borrowVerify,
+  repayBorrowVerify,
+  liquidateBorrowVerify,
+  seizeVerify,
+  transferVerify,
+  // Utility functions
+  getExp,
+  mulExp,
+  mulExp3,
+  divExp,
+  mulScalarTruncate,
+  mulScalarTruncateAddUint,
+  truncate,
 } from './helpers/controller-helper.ts'
 import { SCALAR } from './common.ts'
 
+Clarinet.test({
+  name: 'Testing authorization functions',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get('deployer')
+    if (!deployer) throw new Error('deployer not found')
+    const stoken = `${deployer.address}.stoken`
+    const user1 = accounts.get('wallet_1')
+    const user2 = accounts.get('wallet_2')
+    if (!user1 || !user2) throw new Error('user1 or user2 not found')
+
+    const isControllerResult = await isController(chain, user1.address)
+    isControllerResult.expectBool(true)
+
+    const mintVerifyResult = await mintVerify(chain, user1.address, stoken, stoken, 87n * SCALAR, 10n * SCALAR)
+    mintVerifyResult.expectBool(true)
+
+    const redeemVerifyFailedResult = await redeemVerify(chain, user1.address, stoken, user1.address, SCALAR, 0n)
+    redeemVerifyFailedResult.expectErr().expectUint(4n)
+
+    const redeemVerifySuccessResult = await redeemVerify(chain, user1.address, stoken, user1.address, 12n * SCALAR, 10n * SCALAR)
+    redeemVerifySuccessResult.expectOk().expectBool(true)
+
+    const borrowVerifyResult = await borrowVerify(chain, user1.address, stoken, user1.address, 12n * SCALAR)
+    borrowVerifyResult.expectBool(true)
+
+    const repayBorrowVerifyResult = await repayBorrowVerify(
+      chain,
+      user1.address,
+      stoken,
+      user1.address,
+      user2.address,
+      100n * SCALAR,
+      0n,
+    )
+    repayBorrowVerifyResult.expectBool(true)
+
+    const liquidateBorrowVerifyResult = await liquidateBorrowVerify(
+      chain,
+      user1.address,
+      stoken,
+      stoken,
+      user1.address,
+      user2.address,
+      50n * SCALAR,
+      100n * SCALAR,
+    )
+    liquidateBorrowVerifyResult.expectBool(true)
+
+    const seizeVerifyResult = await seizeVerify(
+      chain,
+      user1.address,
+      stoken,
+      stoken,
+      user1.address,
+      user2.address,
+      100n * SCALAR,
+    )
+    seizeVerifyResult.expectBool(true)
+
+    const transferVerifyResult = await transferVerify(
+      chain,
+      user1.address,
+      stoken,
+      user1.address,
+      user2.address,
+      100n * SCALAR,
+    )
+    transferVerifyResult.expectBool(true)
+  },
+})
+
 // Utility functions
 Clarinet.test({
-    name: 'Testing exponential calculation',
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        const deployer = accounts.get('deployer')
-        if (!deployer) throw new Error('deployer not found')
-        const user1 = accounts.get('wallet_1')
-        if (!user1) throw new Error('user1 not found')
+  name: 'Testing exponential calculation',
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get('deployer')
+    if (!deployer) throw new Error('deployer not found')
+    const user1 = accounts.get('wallet_1')
+    if (!user1) throw new Error('user1 not found')
 
-        const getExpResult = await getExp(chain, user1.address, 4n, 2n)
-        getExpResult.expectUint(2n * SCALAR)
+    const getExpResult = await getExp(chain, user1.address, 4n, 2n)
+    getExpResult.expectUint(2n * SCALAR)
 
-        const mulExpResult = await mulExp(chain, user1.address, 4n * SCALAR, 2n * SCALAR)
-        mulExpResult.expectUint(4n * 2n * SCALAR)
+    const mulExpResult = await mulExp(chain, user1.address, 4n * SCALAR, 2n * SCALAR)
+    mulExpResult.expectUint(4n * 2n * SCALAR)
 
-        const mulExp3Result = await mulExp3(chain, user1.address, 4n * SCALAR, 2n * SCALAR, 8n * SCALAR)
-        mulExp3Result.expectUint(4n * 2n * 8n * SCALAR)
+    const mulExp3Result = await mulExp3(chain, user1.address, 4n * SCALAR, 2n * SCALAR, 8n * SCALAR)
+    mulExp3Result.expectUint(4n * 2n * 8n * SCALAR)
 
-        const divExpResult1 = await divExp(chain, user1.address, 4n * SCALAR, 2n * SCALAR)
-        divExpResult1.expectUint(2n * SCALAR)
+    const divExpResult1 = await divExp(chain, user1.address, 4n * SCALAR, 2n * SCALAR)
+    divExpResult1.expectUint(2n * SCALAR)
 
-        const divExpResult2 = await divExp(chain, user1.address, 4n * SCALAR, 3n * SCALAR)
-        divExpResult2.expectUint(4n * SCALAR / 3n)
+    const divExpResult2 = await divExp(chain, user1.address, 4n * SCALAR, 3n * SCALAR)
+    divExpResult2.expectUint(4n * SCALAR / 3n)
 
-        const mulScalarTruncateResult = await mulScalarTruncate(chain, user1.address, 4n * SCALAR, 2n * SCALAR)
-        mulScalarTruncateResult.expectUint(4n * 2n * SCALAR)
+    const mulScalarTruncateResult = await mulScalarTruncate(chain, user1.address, 4n * SCALAR, 2n * SCALAR)
+    mulScalarTruncateResult.expectUint(4n * 2n * SCALAR)
 
-        const mulScalarTruncateAddUintResult = await mulScalarTruncateAddUint(chain, user1.address, 4n * SCALAR, 2n * SCALAR, 12n)
-        mulScalarTruncateAddUintResult.expectUint(4n * 2n * SCALAR + 12n)
+    const mulScalarTruncateAddUintResult = await mulScalarTruncateAddUint(chain, user1.address, 4n * SCALAR, 2n * SCALAR, 12n)
+    mulScalarTruncateAddUintResult.expectUint(4n * 2n * SCALAR + 12n)
 
-        const truncateResult1 = await truncate(chain, user1.address, 4n * SCALAR)
-        truncateResult1.expectUint(4n)
+    const truncateResult1 = await truncate(chain, user1.address, 4n * SCALAR)
+    truncateResult1.expectUint(4n)
 
-        const truncateResult2 = await truncate(chain, user1.address, 4n * 100_000n)
-        truncateResult2.expectUint(0n)
-    },
+    const truncateResult2 = await truncate(chain, user1.address, 4n * 100_000n)
+    truncateResult2.expectUint(0n)
+  },
 })
--- tests/helpers/controller-helper.ts
+++ tests/helpers/controller-helper.ts
@@ -1,89 +1,275 @@
-import { Chain, types } from 'https://deno.land/x/clarinet@v1.0.2/index.ts'
+import { Chain, Tx, types } from 'https://deno.land/x/clarinet@v1.0.2/index.ts'
 
-const CONTROLLER = 'controller-1'
+const CONTROLLER_CONTRACT = 'controller-1'
+
+export async function isController(
+  chain: Chain,
+  sender: string,
+): Promise<String> {
+  const block = chain.mineBlock([
+    Tx.contractCall(
+      CONTROLLER_CONTRACT, 'is-controller', [], sender
+    )
+  ])
+  return block.receipts[0].result.expectOk()
+}
+
+export async function mintVerify(
+  chain: Chain,
+  sender: string,
+  stoken: string,
+  minter: string,
+  mintAmount: bigint,
+  mintTokens: bigint,
+): Promise<String> {
+  const block = chain.mineBlock([
+    Tx.contractCall(
+      CONTROLLER_CONTRACT,
+      'mint-verify',
+      [
+        types.principal(stoken),
+        types.principal(minter),
+        types.uint(mintAmount),
+        types.uint(mintTokens),
+      ],
+      sender,
+    )
+  ])
+  return block.receipts[0].result.expectOk()
+}
+
+export async function redeemVerify(
+  chain: Chain,
+  sender: string,
+  stoken: string,
+  redeemer: string,
+  redeemAmount: bigint,
+  redeemTokens: bigint,
+): Promise<String> {
+  const block = chain.mineBlock([
+    Tx.contractCall(
+      CONTROLLER_CONTRACT,
+      'redeem-verify',
+      [
+        types.principal(stoken),
+        types.principal(redeemer),
+        types.uint(redeemAmount),
+        types.uint(redeemTokens),
+      ],
+      sender,
+    )
+  ])
+  return block.receipts[0].result
+}
+
+export async function borrowVerify(
+  chain: Chain,
+  sender: string,
+  stoken: string,
+  borrower: string,
+  borrowAmount: bigint,
+): Promise<String> {
+  const block = chain.mineBlock([
+    Tx.contractCall(
+      CONTROLLER_CONTRACT,
+      'borrow-verify',
+      [
+        types.principal(stoken),
+        types.principal(borrower),
+        types.uint(borrowAmount),
+      ],
+      sender,
+    )
+  ])
+  return block.receipts[0].result.expectOk()
+}
+
+export async function repayBorrowVerify(
+  chain: Chain,
+  sender: string,
+  stoken: string,
+  payer: string,
+  borrower: string,
+  repayAmount: bigint,
+  borrowerIndex: bigint,
+): Promise<String> {
+  const block = chain.mineBlock([
+    Tx.contractCall(
+      CONTROLLER_CONTRACT,
+      'repay-borrow-verify',
+      [
+        types.principal(stoken),
+        types.principal(payer),
+        types.principal(borrower),
+        types.uint(repayAmount),
+        types.uint(borrowerIndex),
+      ],
+      sender,
+    )
+  ])
+  return block.receipts[0].result.expectOk()
+}
+
+export async function liquidateBorrowVerify(
+	chain: Chain,
+	sender: string,
+	stokenBorrowed: string,
+  stokenCollateral: string,
+	liquidator: string,
+  borrower: string,
+	repayAmount: bigint,
+	seizeTokens: bigint,
+): Promise<String> {
+	const block = chain.mineBlock([
+		Tx.contractCall(
+			CONTROLLER_CONTRACT,
+      'liquidate-borrow-verify',
+      [
+        types.principal(stokenBorrowed),
+        types.principal(stokenCollateral),
+        types.principal(liquidator),
+        types.principal(borrower),
+        types.uint(repayAmount),
+        types.uint(seizeTokens),
+      ],
+      sender,
+		)
+	])
+	return block.receipts[0].result.expectOk()
+}
+
+export async function seizeVerify(
+	chain: Chain,
+	sender: string,
+  stokenCollateral: string,
+	stokenBorrowed: string,
+	liquidator: string,
+  borrower: string,
+	seizeTokens: bigint,
+): Promise<String> {
+	const block = chain.mineBlock([
+		Tx.contractCall(
+			CONTROLLER_CONTRACT,
+      'seize-verify',
+      [
+        types.principal(stokenCollateral),
+        types.principal(stokenBorrowed),
+        types.principal(liquidator),
+        types.principal(borrower),
+        types.uint(seizeTokens),
+      ],
+      sender,
+		)
+	])
+	return block.receipts[0].result.expectOk()
+}
+
+export async function transferVerify(
+	chain: Chain,
+	sender: string,
+  stoken: string,
+	src: string,
+	dst: string,
+	transferTokens: bigint,
+): Promise<String> {
+	const block = chain.mineBlock([
+		Tx.contractCall(
+			CONTROLLER_CONTRACT,
+      'transfer-verify',
+      [
+        types.principal(stoken),
+        types.principal(src),
+        types.principal(dst),
+        types.uint(transferTokens),
+      ],
+      sender,
+		)
+	])
+	return block.receipts[0].result.expectOk()
+}
 
 // Utility functions
 export async function getExp(
-    chain: Chain,
-    sender: string,
-    num: bigint,
-    denom: bigint,
+  chain: Chain,
+  sender: string,
+  num: bigint,
+  denom: bigint,
 ): Promise<String> {
-    const res = await chain.callReadOnlyFn(
-        CONTROLLER, 'get-exp', [types.uint(num), types.uint(denom)], sender  
-    )
-    return res.result
+  const res = await chain.callReadOnlyFn(
+    CONTROLLER_CONTRACT, 'get-exp', [types.uint(num), types.uint(denom)], sender  
+  )
+  return res.result
 }
 
 export async function mulExp(
-    chain: Chain,
-    sender: string,
-    a: bigint,
-    b: bigint,
+  chain: Chain,
+  sender: string,
+  a: bigint,
+  b: bigint,
 ): Promise<String> {
-    const res = await chain.callReadOnlyFn(
-        CONTROLLER, 'mul-exp', [types.uint(a), types.uint(b)], sender  
-    )
-    return res.result
+  const res = await chain.callReadOnlyFn(
+    CONTROLLER_CONTRACT, 'mul-exp', [types.uint(a), types.uint(b)], sender  
+  )
+  return res.result
 }
 
 export async function mulExp3(
-    chain: Chain,
-    sender: string,
-    a: bigint,
-    b: bigint,
-    c: bigint,
+  chain: Chain,
+  sender: string,
+  a: bigint,
+  b: bigint,
+  c: bigint,
 ): Promise<String> {
-    const res = await chain.callReadOnlyFn(
-        CONTROLLER, 'mul-exp3', [types.uint(a), types.uint(b), types.uint(c)], sender  
-    )
-    return res.result
+  const res = await chain.callReadOnlyFn(
+    CONTROLLER_CONTRACT, 'mul-exp3', [types.uint(a), types.uint(b), types.uint(c)], sender  
+  )
+  return res.result
 }
 
 export async function divExp(
-    chain: Chain,
-    sender: string,
-    a: bigint,
-    b: bigint,
+  chain: Chain,
+  sender: string,
+  a: bigint,
+  b: bigint,
 ): Promise<String> {
-    const res = await chain.callReadOnlyFn(
-        CONTROLLER, 'div-exp', [types.uint(a), types.uint(b)], sender  
-    )
-    return res.result
+  const res = await chain.callReadOnlyFn(
+    CONTROLLER_CONTRACT, 'div-exp', [types.uint(a), types.uint(b)], sender  
+  )
+  return res.result
 }
 
 export async function mulScalarTruncate(
-    chain: Chain,
-    sender: string,
-    exp: bigint,
-    scalar: bigint,
+  chain: Chain,
+  sender: string,
+  exp: bigint,
+  scalar: bigint,
 ): Promise<String> {
-    const res = await chain.callReadOnlyFn(
-        CONTROLLER, 'mul-scalar-truncate', [types.uint(exp), types.uint(scalar)], sender  
-    )
-    return res.result
+  const res = await chain.callReadOnlyFn(
+    CONTROLLER_CONTRACT, 'mul-scalar-truncate', [types.uint(exp), types.uint(scalar)], sender  
+  )
+  return res.result
 }
 
 export async function mulScalarTruncateAddUint(
-    chain: Chain,
-    sender: string,
-    exp: bigint,
-    scalar: bigint,
-    addend: bigint,
+  chain: Chain,
+  sender: string,
+  exp: bigint,
+  scalar: bigint,
+  addend: bigint,
 ): Promise<String> {
-    const res = await chain.callReadOnlyFn(
-        CONTROLLER, 'mul-scalar-truncate-add-uint', [types.uint(exp), types.uint(scalar), types.uint(addend)], sender  
-    )
-    return res.result
+  const res = await chain.callReadOnlyFn(
+    CONTROLLER_CONTRACT, 'mul-scalar-truncate-add-uint', [types.uint(exp), types.uint(scalar), types.uint(addend)], sender  
+  )
+  return res.result
 }
 
 export async function truncate(
-    chain: Chain,
-    sender: string,
-    exp: bigint,
+  chain: Chain,
+  sender: string,
+  exp: bigint,
 ): Promise<String> {
-    const res = await chain.callReadOnlyFn(
-        CONTROLLER, 'truncate', [types.uint(exp)], sender  
-    )
-    return res.result
+  const res = await chain.callReadOnlyFn(
+    CONTROLLER_CONTRACT, 'truncate', [types.uint(exp)], sender  
+  )
+  return res.result
 }
