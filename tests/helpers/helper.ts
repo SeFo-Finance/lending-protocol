bb00fe2c9a350a554defb21ed89d19d09c36bd79
daiwanwei <wadejet.work@gmail.com>
2022-10-24 17:16:11 +0800

    modify ir contract and add ir unit test

--- tests/helpers/helper.ts
+++ tests/helpers/helper.ts
@@ -1,12 +1,37 @@
 import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.2/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
-import { SCALAR } from '../common.ts';
+import {BASE_RATE_PER_BLOCK_MANTISSA, MULTIPLIER_PER_BLOCK_MANTISSA, SCALAR} from '../common.ts';
 
 
+export function calculateUtilizationRate(
+    cash:bigint,borrows:bigint,reserves:bigint
+):bigint{
+    if (borrows===0n) return 0n
+    return (borrows)*SCALAR/(cash+borrows-reserves)
+}
+
+export function calculateBorrowRate(
+    cash:bigint,borrows:bigint,reserves:bigint
+):bigint{
+    const ur=calculateUtilizationRate(cash,borrows,reserves)
+    return (ur*MULTIPLIER_PER_BLOCK_MANTISSA)/SCALAR+BASE_RATE_PER_BLOCK_MANTISSA
+}
+
+export function calculateSupplyRate(
+    cash:bigint,borrows:bigint,
+    reserves:bigint,reserveFactorMantissa:bigint,
+):bigint{
+    const factor=SCALAR-reserveFactorMantissa
+    const borrowRate=calculateBorrowRate(cash,borrows, reserves)
+    const rateToPool=borrowRate*factor/SCALAR
+    const ur=calculateUtilizationRate(cash,borrows,reserves)
+    return (ur*rateToPool)/SCALAR
+}
 
 export function calculateExchangeRate(
     cash:bigint,borrows:bigint,reserves:bigint,supply:bigint
     ):bigint{
+    if (supply===0n) return 0n
     return (cash+borrows-reserves)*SCALAR/supply
 }
 
